3e88c71ba622676b0d51837217956eba2d5372d2
Gagan-rai <gaganrai2246@gmail.com>
2026-05-14 19:26:38 +0530

    this chnge occur , const frontendBuildPath = path.join(__dirname, '../client/dist');

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,14 +4,14 @@ const connectDB = require('./src/config/database');
 const express = require('express'); // Added to use express.static
 const path = require('path');       // Added to resolve file paths safely
 const app2 = require('./src/app');
-
+const frontendBuildPath = path.join(__dirname, '../client/dist');
 
 connectDB();
 
-app2.use(express.static(path.join(__dirname, 'client/dist')));
+app2.use(express.static(frontendBuildPath));
 
 app2.get('/*splat', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+  res.sendFile(path.join(frontendBuildPath, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
