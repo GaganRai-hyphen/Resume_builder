c5029eeaca0b181a658a6179c60776a25537d8b9
Gagan-rai <gaganrai2246@gmail.com>
2026-05-14 18:48:39 +0530

    chnge name of const app to app2

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,20 +3,20 @@ const app = require('./src/app');
 const connectDB = require('./src/config/database');
 const express = require('express'); // Added to use express.static
 const path = require('path');       // Added to resolve file paths safely
-const app = require('./src/app');
+const app2 = require('./src/app');
 
 
 connectDB();
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app2.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.get('*', (req, res) => {
+app2.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app2.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
