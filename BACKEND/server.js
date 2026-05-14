358f580ddeb3354655f5458fe3aa70eac46abdb3
Gagan-rai <gaganrai2246@gmail.com>
2026-05-14 18:39:50 +0530

    these chnges occur in server.js , const express = require('express'); // Added to use express.static const path = require('path');       // Added to resolve file paths safely const app = require('./src/app');
    app.use(express.static(path.join(__dirname, 'client/dist')));
    
    app.get('*', (req, res) => {
      res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
    });

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,9 +1,18 @@
 require('dotenv').config(); 
 const app = require('./src/app');
 const connectDB = require('./src/config/database');
+const express = require('express'); // Added to use express.static
+const path = require('path');       // Added to resolve file paths safely
+const app = require('./src/app');
 
 
 connectDB();
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
