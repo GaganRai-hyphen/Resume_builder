77d1d5ec4cc4a1bb9241cab4d4eab51f1bf1d837
Gagan-rai <gaganrai2246@gmail.com>
2026-05-14 19:40:38 +0530

    this chnge ,  const frontendDist = path.join(__dirname, '../frontend/dist'); app.use(express.static(frontendDist));
    app.get('/*splat', (req, res) => {
      res.sendFile(path.join(frontendDist, 'index.html'));
    });

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,14 +4,14 @@ const connectDB = require('./src/config/database');
 const express = require('express'); // Added to use express.static
 const path = require('path');       // Added to resolve file paths safely
 const app2 = require('./src/app');
-const frontendBuildPath = path.join(__dirname, '../client/dist');
+
 
 connectDB();
 
-const frontendDist = path.join(__dirname, '../FRONTEND/dist');
-app2.use(express.static(frontendDist));
+const frontendDist = path.join(__dirname, '../frontend/dist');
+app.use(express.static(frontendDist));
 
-app2.get('/*splat', (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(frontendDist, 'index.html'));
 });
 
